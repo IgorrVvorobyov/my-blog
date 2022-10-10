24b4f431e15d12f5c49f9d30dc1127ca37ad0400
igorvorobev <vorobyovigorr@gmail.com>
2022-10-10 18:59:23 +0300

    create admin sanity

--- dev/previews.jsx
+++ dev/previews.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Previews } from '@react-buddy/ide-toolbox';
-import { PaletteTree } from './palette';
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
+import {PaletteTree} from './palette';
+import Home from "../pages";
 
 const ComponentPreviews = () => {
     return (
-        <Previews palette={<PaletteTree />}>
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Home">
+                <Home/>
+            </ComponentPreview>
         </Previews>
     );
 };
